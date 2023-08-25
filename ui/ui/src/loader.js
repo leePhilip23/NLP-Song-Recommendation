de6c368dddd159cb54d521720e07eaf5aa2c481c
justin-doan <jmdoan8@gmail.com>
2023-08-24 23:31:53 -0500

    changes to UI, added loading animation

---
+++ ui/ui/src/loader.js
@@ -0,0 +1,14 @@
+import {ThreeDots} from "react-loader-spinner";
+const LoaderComp = () => {
+ 
+    return (
+        <ThreeDots
+            color="rgb(155, 236, 34)"
+            height={70}
+            width={70}
+            timeout={5000}
+        />
+ 
+    );
+}
+export default LoaderComp;
