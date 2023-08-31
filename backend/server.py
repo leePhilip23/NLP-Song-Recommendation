743a86d4b76c594b828148face625968a9f44eb2
Philip Lee <74793573+leePhilip23@users.noreply.github.com>
2023-08-31 10:48:04 -0500

    Got rid of redundant pycaches

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 from webbrowser import get
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from ML import get_summary, get_article
+from util_ML import get_summary, get_article
 
 # import time # use to test loading animation
 app = FastAPI()
@@ -26,10 +26,16 @@ stored_test = ""
 async def post_text(request_data: dict):
     global stored_text
     stored_text = request_data.get("text", "")
+
+    # Get the parsed article
     article = get_article(stored_text)
+
+    # Get the summary
     summary = get_summary(article)
+
     # time.sleep(1) # use to test loading animation
     return {"summary": summary}
+
 
 @app.get("/get_text/")
 async def get_text():
@@ -37,6 +43,7 @@ async def get_text():
         return stored_text
     else:
         return "No text available."
+
 
 @app.get("/")
 async def testing_endpoint():
--- backend/util_ML.py
+++ backend/util_ML.py
@@ -1,12 +1,14 @@
 from transformers import AutoTokenizer
 from peft import AutoPeftModelForSeq2SeqLM
 from newspaper import Article
+import nltk
+
+'''
 import os
 
 # You can also do export KMP_DUPLICATE_LIB_OK=True on terminal
-# Only on mac os
-# os.environ["KMP_DUPLICATE_LIB_OK"]="True"
-
+# os.environ["KMP_DUPLICATE_LIB_OK"]="True" # Only for MAC OS
+'''
 # Model Id and Token Id used
 model_id = "../backend/Flan-T5-Small_Lora"
 token_id  = "google/flan-t5-small"
@@ -16,6 +18,27 @@ model = AutoPeftModelForSeq2SeqLM.from_pretrained(model_id)
 tokenizer = AutoTokenizer.from_pretrained(token_id)
 
 
+# Tokenize article
+def tokenize(article):
+   sentences = nltk.sent_tokenize(article)
+
+   num_tokens = 0 
+   paragraph = ''
+   for s in sentences:
+      # Tokenize each sentence and join it as one string
+      words = nltk.word_tokenize(s)
+      paragraph += ' '.join(words)
+      num_tokens += len(words)
+
+      # Break if there's more than 450 tokens and there's a sentence
+      if num_tokens >= 450 and words[-1] == '.':
+         break
+   
+   # Return paragraph
+   return paragraph
+
+
+# Download article and parse through it
 def get_article(link):
    #For different language newspaper refer above table
    toi_article = Article(link, language="en") # en for English
@@ -29,23 +52,34 @@ def get_article(link):
    # Get Summary
    article = toi_article.text
 
-   return article
+   # Get shortened summary
+   short_article = tokenize(article)
+
+   return short_article
 
 
+# Get summary of article
 def get_summary(input_text):
    # Tokenize input text
-   tokens_input = tokenizer.encode("Summarize: "+ input_text, return_tensors='pt',
-                                   max_length=tokenizer.model_max_length,
-                                   truncation=True)
+   tokens_input = tokenizer.encode(
+      "Summarize: " + input_text,
+      return_tensors='pt',
+      max_length=tokenizer.model_max_length,
+      truncation=True
+   )
 
    # Generate summary
-   summary_ids = model.generate(input_ids=tokens_input,
-                                min_length=80,
-                                max_length=150,
-                                length_penalty=20, 
-                                num_beams=2)
+   summary_ids = model.generate(
+      input_ids=tokens_input, 
+      max_length=150,
+      bos_token_id=0
+   )
 
    # Decode and print the summary
-   summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+   summary = tokenizer.decode(
+      summary_ids[0],
+      skip_special_tokens=True
+   )
 
+   # Return finalized summary
    return summary
